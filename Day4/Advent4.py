99e64627643b9d72c978a7ad70fe6b12daf3dff7
mrogove <mrogove@gmail.com>
2017-12-11 22:15:09 -0500

    Day4/Advent4.py commit

--- Day4/Advent4.py
+++ Day4/Advent4.py
@@ -1,3 +1,12 @@
+results = []
 with open("Advent4input.txt") as inputfile:
     for line in inputfile:
         results.append(line.strip().split(' '))
+
+validsum = 0
+
+for array in results: #for each list in the list of lists
+    if len(array) == len(set(array)):
+        validsum += 1
+
+print(validsum)
