cf2bae7aa7c0c6fb1a52132bf74ddcf9344f418b
mrogove <mrogove@gmail.com>
2017-12-11 23:00:22 -0500

    Updating Day4/Advent4.py to include part 2

--- Day4/Advent4.py
+++ Day4/Advent4.py
@@ -1,12 +1,19 @@
+import itertools
+
 results = []
 with open("Advent4input.txt") as inputfile:
     for line in inputfile:
         results.append(line.strip().split(' '))
 
 validsum = 0
+noanagram = 0
 
 for array in results: #for each list in the list of lists
     if len(array) == len(set(array)):
         validsum += 1
 
-print(validsum)
+    if (len(array) == len(set(["".join(sorted(word)) for word in array]))):
+        noanagram += 1
+
+print(validsum) #part1
+print(noanagram) #part2
