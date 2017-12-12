30509295d58976d747895083c466ac26fe3e8543
mrogove <mrogove@gmail.com>
2017-12-11 23:03:43 -0500

    adding better commenting to explain solution for Advent4.py part 2

--- Day4/Advent4.py
+++ Day4/Advent4.py
@@ -12,6 +12,9 @@ for array in results: #for each list in the list of lists
     if len(array) == len(set(array)):
         validsum += 1
 
+#for each array: for each word, use #sorted# to alphebatize each string
+    #then, use "".join to join each alphebatized string together
+    #then, take the set of these; compare set of each array with each array
     if (len(array) == len(set(["".join(sorted(word)) for word in array]))):
         noanagram += 1
 
