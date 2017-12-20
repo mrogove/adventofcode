2bf54717320c4d59f72533a785032338ca280a3c
mrogove <mrogove@gmail.com>
2017-12-20 11:51:13 -0500

    puzzled out answer to part2, day 6

--- Day6/Advent6.py
+++ Day6/Advent6.py
@@ -8,9 +8,14 @@ while valbankList not in seenVals:
     m = max(valbankList)
     x = valbankList.index(m)
     valbankList[x] = 0 #also reset to 0!
-    while m:
+    while m: #inside the mainlist
         x=(x+1)%len(valbankList)
         valbankList[x]+=1
         m-=1 #I hope
 
+#once the seenVals contains valbankList value, breaks loop:
 print (len(seenVals))
+#to get part 2, need to find how long between cycles.
+#the duplicate state won't have its index overwritten;
+    #can use index value of first instance:
+print (len(seenVals)-seenVals.index(valbankList))
