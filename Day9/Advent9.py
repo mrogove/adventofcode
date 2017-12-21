95267b46d89b705afd2fd67279ea21b1942af591
mrogove <mrogove@gmail.com>
2017-12-21 11:45:11 -0500

    adding in Day9 part 2; garbage counting.

--- Day9/Advent9.py
+++ Day9/Advent9.py
@@ -3,6 +3,7 @@ with open('Advent9input.txt') as input_file:
 
 score = 0
 current_depth = 0
+garbage_count = 0 #needed for part 2
 #init flags; assumes first char fine
 inside_garbage = False
 skip_char = False
@@ -14,13 +15,16 @@ for char in textline:
             skip_char = True
         elif char == ">":
             inside_garbage = False
+        else:
+            garbage_count += 1
     else:  # when inside group, not garbage
         if char == "{": #add to depth inside bracket
             current_depth += 1
         elif char == "}":
             score += current_depth
-            current_depth -= 1
+            current_depth -= 1 #move up one
         elif char == "<":
             inside_garbage = True
 
 print("Part 1:   ", score)
+print("Part 2:   ", garbage_count)
